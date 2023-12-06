ffeb77cab42a24097b571111673d375174841ca0
akira21kaji <arkamiyama21@gmail.com[alias]>
2023-12-06 15:41:45 +0900

    中級編完了

---
+++ javascript編/src/js/main.js
@@ -0,0 +1,105 @@
+// console.log("ZeroPlusGate");
+
+// console.log(1);
+// console.log("1");
+
+// console.log(1 + 4);
+// console.log(3 - 2);
+// console.log(2 * 2);
+// console.log(6 / 2);
+
+// console.log("ZeroPlus" + "Gate");
+
+// 変数
+// var zp = "ZeroPlus";
+// console.log(zp);
+
+// // var 変数名 = 値;
+
+// let zpg = "ZeroPlus Gate";
+// console.log(zpg);
+
+// 再代入
+// var fruit = "りんご";
+// fruit = "バナナ";
+
+// // 再定義
+// var fruit = "パイナップル";
+// console.log(fruit);
+
+// let fruit = "りんご";
+
+// fruit = "バナナ";
+
+// let fruit = "パイナップル";
+// console.log(fruit);
+
+// console.log(1200 + 2200);//3400
+// console.log(3200 + 800);//4200
+// console.log(4000 + 4100);//8300
+
+// let total = 0;
+// total = 1200;
+// total = total + 2200;
+// total = total + 800;
+// total = total + 4100;
+
+// console.log(total);
+
+//定数
+// const zp = "ZeroPlus";
+
+// console.log(zp + "Gate");
+// console.log(zp + "Career");
+// console.log(zp + "Club");
+
+// 比較演算子
+// console.log(10 < 5); //false
+
+// console.log(10 <= 5); //false
+
+// console.log(10 > 5); // true
+
+// console.log(10 >= 5); // true
+
+// console.log(10 == 10);
+
+// console.log(10 === 5);
+
+// console.log("10" == 10); // true
+
+// console.log("10" === 10); // false
+
+// console.log(10 != 5); // true
+
+// console.log("10" !== 10);
+
+// 論理演算子
+
+// console.log(10 > 5 && 5 > 10);
+
+// console.log(10 > 5 || 5 > 10);
+
+// console.log(!10 < 5);
+
+// if
+// let age = 100;
+
+// if (age == 19) {
+//   console.log("正しい");
+// } else if (age < 60) {
+//   console.log("60より小さい");
+// } else {
+//   console.log("正しくない");
+// }
+
+// 例題
+let age = 9;
+
+if (age >= 60) {
+  console.log("シニア料金です");
+} else if (age >= 18) {
+  console.log("一般料金です");
+} else {
+  console.log("子ども料金です");
+}
