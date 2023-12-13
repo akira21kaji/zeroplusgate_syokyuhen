113a45584c733d61de0d695c8d78b20f497c78d1
akira21kaji <arkamiyama21@gmail.com[alias]>
2023-12-13 18:44:13 +0900

    JS本すべて完了しました！

--- 01-demo/js/script.js
+++ 01-demo/js/script.js
@@ -1,3 +1,80 @@
+// 監視対象が範囲内に現れたら実行する動作
+const animateFade = (entries, obs) => {
+  entries.forEach((entrie) => {
+    if (entrie.isIntersecting) {
+      // console.log(entrie.target);
+      entrie.target.animate(
+        {
+          opacity: [0, 1],
+          filter: ["blur(.4rem)", "blur(0)"],
+          translate: ["0 4rem", 0],
+        },
+        {
+          duration: 2000,
+          easing: "ease",
+          fill: "forwards",
+        }
+      );
+      // 一度ふわっとしたら動作をやめる
+      obs.unobserve(entrie.target);
+    }
+  });
+};
+
+// 監視設定
+const fadeObserver = new IntersectionObserver(animateFade);
+
+// .fadeinを監視するよう指示
+const fadeElements = document.querySelectorAll(".fadein");
+fadeElements.forEach((fadeElement) => {
+  fadeObserver.observe(fadeElement);
+});
+
+// メニュー関連
+const menuOpen = document.querySelector("#menu-open");
+const menuClose = document.querySelector("#menu-close");
+const menuPanel = document.querySelector("#menu-panel");
+const menuItems = document.querySelectorAll("#menu-panel li");
+const menuOptions = {
+  duration: 1400,
+  easing: "ease",
+  fill: "forwards",
+};
+
+// メニューを開く
+menuOpen.addEventListener("click", () => {
+  // console.log("メニューを開く");
+  menuPanel.animate({ translate: ["100vw", 0] }, menuOptions);
+
+  menuItems.forEach((menuItem, index) => {
+    // console.log(`${index}番目のリスト`);
+    // console.log(menuItem);
+    menuItem.animate(
+      {
+        opacity: [0, 1],
+        translate: ["2rem", 0],
+      },
+      {
+        duration: 2400,
+        delay: 300 * index,
+        easing: "ease",
+        fill: "forwards",
+      }
+    );
+  });
+});
+
+// メニューを閉じる
+menuClose.addEventListener("click", () => {
+  menuPanel.animate({ translate: [0, "100vw"] }, menuOptions);
+  menuItems.forEach((menuItem) => {
+    menuItem.animate({ opacity: [1, 0] }, menuOptions);
+  });
+});
+
+// リンクをひとつずつ順に表示
+
+// ローディング画面
 const loadingAreaGrey = document.querySelector("#loading");
 const loadingAreaGreen = document.querySelector("#loading-screen");
 const loadingText = document.querySelector("#loading p");
